49ad4175146215d0da6564aba203a49a670384d2
5aim <hyukjin720@gmail.com>
2023-09-04 16:28:45 +0900

    Raster Layers - Tiled Raster Layers

--- main.js
+++ main.js
@@ -1,82 +1,112 @@
-window.onload = init
+window.onload = init;
 
 function init() {
-    const CHEONAN = [14152187.20776864, 4413944.543811761];
+  const CHEONAN = [14152187.20776864, 4413944.543811761];
 
-    // Controls
-    const fullscreenControl = new ol.control.FullScreen();
-    const mousePositionControl = new ol.control.MousePosition();
-    const overViewMapControl = new ol.control.OverviewMap({
-        collapsed: false,
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.OSM()
-            })
-        ]
-    })
+  // Controls
+  const fullscreenControl = new ol.control.FullScreen();
+  const mousePositionControl = new ol.control.MousePosition();
+  const overViewMapControl = new ol.control.OverviewMap({
+    collapsed: false,
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+    ],
+  });
 
-    const scaleLineControl = new ol.control.ScaleLine();
-    const zoomSliderControl = new ol.control.ZoomSlider();
+  const scaleLineControl = new ol.control.ScaleLine();
+  const zoomSliderControl = new ol.control.ZoomSlider();
 
-    const map = new ol.Map({
-        view: new ol.View({
-            center: CHEONAN,
-            zoom: 13,
-            maxZoom: 17,
-            minZoom: 13,
-            rotation: 0
+  const map = new ol.Map({
+    // 뷰 지정
+    view: new ol.View({
+      center: CHEONAN,
+      zoom: 13,
+      maxZoom: 17,
+      minZoom: 13,
+      // cartesian coordinate system : 맵 범위 지정
+      extent: [14136311.278827626, 4394656.463022412, 14174402.542814588, 4429267.881654042]
+    }),
+
+    // 레이어 지정
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+        zIndex: 1,
+        visible: true,
+      })
+    ],
+
+    // 컨트롤 지정
+    target: "js-map",
+    keyboardEventTarget: document,
+    controls: ol.control
+      .defaults()
+      .extend([
+        fullscreenControl,
+        mousePositionControl,
+        overViewMapControl,
+        scaleLineControl,
+        zoomSliderControl,
+      ]),
+  });
+
+  // Layer Group
+  const layerGroup = new ol.layer.Group({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM({
+          url: "https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
         }),
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.OSM()
-            })
-        ],
-        target: 'js-map',
-        keyboardEventTarget: document,
-        controls: ol.control.defaults().extend([
-            fullscreenControl,
-            mousePositionControl,
-            overViewMapControl,
-            scaleLineControl,
-            zoomSliderControl,
-        ])
-    })
+        zIndex: 0,
+        visible: false,
+      }),
+    ],
+  });
 
-    // 맵 클릭시 coordinate를 보여줌.
-    // const popupContainerElement = document.getElementById("popup-coordinates");
-    // const popup = new ol.Overlay({
-    //     element : popupContainerElement,
-    //     positioning: 'top-left'
-    // })
+  map.addLayer(layerGroup);
 
-    // map.addOverlay(popup);
+  // 마우스 클릭시 위치의 coordinate를 console.log
+  map.on("click", function (e) {
+    console.log(e.coordinate);
+  });
 
-    // map.on('click', function(e){
-    //     // console.log(e);
-    //     const clickedCoordinate = e.coordinate;
-    //     popup.setPosition(undefined);
-    //     popup.setPosition(clickedCoordinate);
-    //     popupContainerElement.innerHTML = clickedCoordinate;
-    // })
+  // // 맵에서 마우스 위치에 따라 coordinate를 보여줌.
+  // const popupContainerElement = document.getElementById("popup-coordinates");
+  // const popup = new ol.Overlay({
+  //     element : popupContainerElement,
+  //     positioning: 'top-left'
+  // })
 
-    // // DragRotate Interaction
-    // const dragRotateInteraction = new ol.interaction.DragRotate({
-    //     condition : ol.events.condition.altKeyOnly
-    // })
+  // map.addOverlay(popup);
 
-    // map.addInteraction(dragRotateInteraction)
+  // map.on('click', function(e){
+  //     // console.log(e);
+  //     const clickedCoordinate = e.coordinate;
+  //     popup.setPosition(undefined);
+  //     popup.setPosition(clickedCoordinate);
+  //     popupContainerElement.innerHTML = clickedCoordinate;
+  // })
 
-    //// Draw polygon
-    // const drawInteraction = new ol.interaction.Draw({
-    //     type: 'Polygon',
-    //     freehand: true
-    // })
-    // map.addInteraction(drawInteraction);
-    
-    //// polygon의 coordinate를 추출하기
-    // drawInteraction.on('drawend', function(e){
-    //     let parser = new ol.format.GeoJSON();
-    //     let drawnFeatures = parser.writeFeatures([e.feature]);
-    //     console.log(drawnFeatures);
-    // })
+  // // DragRotate Interaction
+  // const dragRotateInteraction = new ol.interaction.DragRotate({
+  //     condition : ol.events.condition.altKeyOnly
+  // })
+
+  // map.addInteraction(dragRotateInteraction)
+
+  // // Draw polygon
+  // const drawInteraction = new ol.interaction.Draw({
+  //     type: 'Polygon',
+  //     freehand: true
+  // })
+  // map.addInteraction(drawInteraction);
+
+  // // polygon의 coordinate를 추출하기
+  // drawInteraction.on('drawend', function(e){
+  //     let parser = new ol.format.GeoJSON();
+  //     let drawnFeatures = parser.writeFeatures([e.feature]);
+  //     console.log(drawnFeatures);
+  // })
 }
